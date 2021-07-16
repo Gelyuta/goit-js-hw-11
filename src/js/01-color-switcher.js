33b2f400502d8f0f0ff462a5737350b95a060126
Anastasia Gelyuta <gelyuta@gmail.com>
2021-07-17 01:55:54 +0300

    Задание 1 - переключатель цветов - выполнено

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+// Получаю доступ к кнопкам
+
+  const refs = {
+    btnStart : document.querySelector('button[data-start]'),
+    btnStop : document.querySelector('button[data-stop]'),
+}
+
+// Вешаю слушатель событий на кнопки
+
+refs.btnStart.addEventListener('click', onClickBtnStart);
+refs.btnStop.addEventListener('click', onClickBtnStop);
+
+let timerId = null;
+
+// Функция, которая меняет цвет боди при клике на кнопку 'start'
+ function onClickBtnStart () {
+    refs.btnStart.disabled = true;
+
+    timerId = setInterval(() => {
+        const colors = getRandomHexColor();
+        document.body.style.backgroundColor = colors;
+    }, 1000);
+
+    console.log('старт')
+}
+
+// Функция, которая останавлявает смену цвета боди при клике на кнопку 'stop'
+ function onClickBtnStop () {
+  refs.btnStart.disabled = false;
+  clearInterval(timerId);
+  console.log('стоп')
+ }
 
